8389eb0b594a4f8548a15780312d455060980645
Robert Daub <rjd2066@gmail.com>
2020-04-03 16:19:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a = 3, b = 3) {
+  return (a + b);
+}
+
+function subtract(a = 10, b = 7) {
+  return (a - b);
+}
+
+function multiply(a = 4, b = 4) {
+  return (a * b);
+}
+
+function divide(a = 9, b = 3) {
+  return (a / b);
+}
